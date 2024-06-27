de5b85a78b01b23982b975d89c8a685f6a136d62
shadowlaq <38447226+shadowlaq@users.noreply.github.com>
2024-06-27 13:24:42 -0700

    nuked second iteration method of POS trigram dedup and found collections library from python. Implemented much faster process.

--- script.py
+++ script.py
@@ -6,19 +6,54 @@ from nltk.util import trigrams
 from nltk import pos_tag, word_tokenize
 import csv
 import re
+from collections import Counter 
+
 
 # Open text file to clean/preprocess
 f = open('Texts/test.txt','r')
 #Read text, lowercase, and clean using regex import
-text = re.sub(r'[-\(\)\"#\/@;:<>\{\}\-=~|\.\?]', '',f.read().lower())
+text = re.sub(r'[-\(\)\"#\/@;:<>\,\{\}\-=~|\.\?\!\*\[\]\']', '',f.read().lower())
 # TRIGRAM and POS list and occurrence tally.
 # Create CSV list and POS Frequency Distribution array in terms of trigrams
-# This is a slow process...expect delays
-for pos in pos_tag(word_tokenize(text)) :
-    # Add to trigram array OR uptick occurrnece
-    # Add to Frequency Distribution array
-    print(pos)
+t_grams = []
+pos_f = [] 
+# This is a slow process...expect delays for tokenize process (6s+)
+print('-----ENTERING POS TOKENIZE PROCESS------')
+print('-----Approx Wait 10s ------')
+pos_t = pos_tag(word_tokenize(text))
+# Loop through POS to populate trigram array and POS freq. array
+# Needed variables
+found = False
+m_sep = " "
+print('-----ENTERING TRIGRAM UNIQUE/COUNT PHASE------')
+print('-----Approx Wait 10min ------')
+# res = Counter(map(''.join, zip(test_str, test_str[1:]))) 
+freq_tri = {}
+freq_pos_tri = {}
+freq_pos_uni = {}
 
+for i in range(len(pos_t)):
+    # Create frequency map of trigrams and tri-POS
+    if i < len(pos_t)-2:
+        trigram = m_sep.join((pos_t[i][0],pos_t[i+1][0],pos_t[i+2][0]))
+        pos_tri = m_sep.join((pos_t[i][1],pos_t[i+1][1],pos_t[i+2][1]))
+        if trigram in freq_tri:
+            freq_tri[trigram] += 1
+        else:
+            freq_tri[trigram] = 1
+
+        if pos_tri in freq_pos_tri:
+            freq_pos_tri[pos_tri] += 1
+        else:
+            freq_pos_tri[pos_tri] = 1    
+    # Create Frequency map of POS Uni
+    pos_uni = pos_t[i][1]
+    if pos_uni in freq_pos_uni:
+        freq_pos_uni[pos_uni] += 1
+    else:
+        freq_pos_uni[pos_uni] = 1
+
+print("The Trigram Frequency is : " + str(freq_pos_tri)) 
 
 f.close()
 
